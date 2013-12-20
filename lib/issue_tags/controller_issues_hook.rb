d163af321e7676b55b02a70d344d33bc9d4693a1
Turchenko Alexander <turchenko.alexander@gmail.com>
2013-12-20 19:41:28 +0400

    fix bulk update tags issue
    now tags save only when user set some value for tags

--- lib/issue_tags/controller_issues_hook.rb
+++ lib/issue_tags/controller_issues_hook.rb
@@ -10,7 +10,7 @@ module IssueTags
     
     def controller_issues_bulk_edit_before_save(context={})
       if context[:params] && context[:params][:issue] && context[:issue]
-        context[:issue][:savable_tags] = context[:params][:issue][:tags] unless context[:params][:issue][:tags].nil?
+        context[:issue][:savable_tags] = context[:params][:issue][:tags] unless context[:params][:issue][:tags].to_s.empty?
       end
     end
 
